a00688a2851e760cd0986eae0045140dcf127681
Zankhana Patel <41522189+Zankhana1995@users.noreply.github.com>
2019-02-23 00:48:13 -0500

    Update EReport.java
    changes in file reading

--- src/eReport/EReport.java
+++ src/eReport/EReport.java
@@ -8,9 +8,10 @@ public interface EReport {
 	public static void main(String[] args) throws IOException {
 		String[] lines = Files.readAllLines(new File("employees.dat").toPath()).toArray(new String[0]);
 		for(String line: lines) {
-		    int courseId = Integer.parseInt(line.substring(0, 4));
-		    String studentName = line.substring(4, 24).trim();
+		   // int courseId = Integer.parseInt(line.substring(0, 4));
+		   // String studentName = line.substring(4, 24).trim();
 		    // etc...
+			System.out.println(line);
 		}
 	}
 }
