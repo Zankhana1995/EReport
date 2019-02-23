94258023e6d17d8eef2b45c9e3af8e1757e8f3da
Zankhana Patel <41522189+Zankhana1995@users.noreply.github.com>
2019-02-23 01:43:51 -0500

    Update EReport.java
    Sort by employeeId (String contained integer)

--- src/eReport/EReport.java
+++ src/eReport/EReport.java
@@ -3,15 +3,35 @@ package eReport;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public interface EReport {
 	public static void main(String[] args) throws IOException {
 		String[] lines = Files.readAllLines(new File("employees.dat").toPath()).toArray(new String[0]);
-		for(String line: lines) {
-		   // int courseId = Integer.parseInt(line.substring(0, 4));
-		   // String studentName = line.substring(4, 24).trim();
-		    // etc...
-			System.out.println(line);
+		List<String> list = new ArrayList<>();		
+		for(String line: lines) {		 
+			if(!line.startsWith("#"))
+			{
+				list.add(line);
+			}			
 		}
+		sortByEmployeeNumber(list);
+	 
+	}
+
+	public static void sortByEmployeeNumber(List<String> list) {
+		Map<Integer, String> map = new TreeMap<Integer, String>();
+		for (String line : list) {
+			int employeeNumber = Integer.parseInt(line.substring(0, 3));
+			map.put(employeeNumber, line.substring(4));
+		}
+		for(Map.Entry<Integer,String> entry : map.entrySet())
+		{
+			System.out.println(entry.getKey()+","+entry.getValue());
+		}
+		
 	}
 }
