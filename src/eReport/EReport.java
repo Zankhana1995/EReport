219ef418c3e237886998314ada43ae895ab4b7a3
Zankhana Patel <41522189+Zankhana1995@users.noreply.github.com>
2019-02-23 02:13:36 -0500

    Update EReport.java
    sorted by empployees last  name using treemap.

--- src/eReport/EReport.java
+++ src/eReport/EReport.java
@@ -11,15 +11,29 @@ import java.util.TreeMap;
 public interface EReport {
 	public static void main(String[] args) throws IOException {
 		String[] lines = Files.readAllLines(new File("employees.dat").toPath()).toArray(new String[0]);
-		List<String> list = new ArrayList<>();		
-		for(String line: lines) {		 
-			if(!line.startsWith("#"))
-			{
+		List<String> list = new ArrayList<>();
+		for (String line : lines) {
+			if (!line.startsWith("#")) {
 				list.add(line);
-			}			
+			}
 		}
 		sortByEmployeeNumber(list);
-	 
+		System.out.println("\n");
+		sortByEmployeeLastName(list);
+	}
+
+	public static void sortByEmployeeLastName(List<String> list) {
+		TreeMap<String, String> hmap = new TreeMap<>();
+		for (String line : list) {
+			String[] elements = line.split(",");
+			String[] fullname = elements[1].trim().split("\\s+");
+			hmap.put(fullname[1], elements[0]+","+elements[1].trim());
+		}
+		System.out.println("Processing by last (family) Name...");
+		for (Map.Entry<String, String> entry : hmap.entrySet()) {
+			System.out.println(entry.getValue().trim());
+		}
+
 	}
 
 	public static void sortByEmployeeNumber(List<String> list) {
@@ -28,10 +42,9 @@ public interface EReport {
 			int employeeNumber = Integer.parseInt(line.substring(0, 3));
 			map.put(employeeNumber, line.substring(4));
 		}
-		for(Map.Entry<Integer,String> entry : map.entrySet())
-		{
-			System.out.println(entry.getKey()+","+entry.getValue());
+		System.out.println("Processing by employee number...");
+		for (Map.Entry<Integer, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + "," + entry.getValue().trim());
 		}
-		
 	}
 }
