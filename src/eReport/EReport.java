e4d6ad3aa593b157d0aceb4a21425a80393e1802
Zankhana Patel <41522189+Zankhana1995@users.noreply.github.com>
2019-02-23 00:45:50 -0500

    Update EReport.java
    read data from file

--- src/eReport/EReport.java
+++ src/eReport/EReport.java
@@ -1,5 +1,16 @@
 package eReport;
 
-public interface EReport {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
+public interface EReport {
+	public static void main(String[] args) throws IOException {
+		String[] lines = Files.readAllLines(new File("employees.dat").toPath()).toArray(new String[0]);
+		for(String line: lines) {
+		    int courseId = Integer.parseInt(line.substring(0, 4));
+		    String studentName = line.substring(4, 24).trim();
+		    // etc...
+		}
+	}
 }
